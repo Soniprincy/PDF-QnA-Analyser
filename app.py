d0804d6ead6b378240d948713e69089988ef6be9
PRINCY SONI <soniprincy02@gmail.com>
2025-08-11 00:00:25 +0530

    '.'

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ def get_conversational_chain():
 
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="models/gemini-1.5-flash-latest", temperature=0.3)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     return load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
